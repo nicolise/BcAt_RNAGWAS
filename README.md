240176ed1c2d9fec30a2ed300bbf26a28c3f05bc
Nicole E Soltis <ne.soltis@gmail.com>
2017-09-22 11:56:25 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BcAt_RNAGWAS
+
+do NOT back up image files or data onto github- use S drive for that
+to avoid this, add image/ figure/ data/ etc. subfolders to .gitignore
---
+++ scripts/bigRRGWAS/01_NameIsolates.R
@@ -0,0 +1,22 @@
+#092217
+#Nicole E Soltis
+#01_NameIsolates.R
+#short script to match phenotypes of Isolates with IDs 1-97 with actual Isolate names for matching to SNP data.
+
+#-----------------------------------------------------------------
+#load data
+setwd("~/Projects/BcAt_RNAGWAS/data")
+IsoNames <- read.csv("IsolateKey_Vivian.csv")
+BotReads <- read.csv("BcBOT_result.lsm.csv")
+
+#each plant x transcript is a unique phenotype
+BotReads.w <- reshape(BotReads, idvar = "Isolate", timevar = "HostGenotype", direction = "wide")
+
+#attach Isolate Names
+names(IsoNames)[1] <- "Isolate"
+BotReads.f <- merge(BotReads.w, IsoNames, by="Isolate")
+
+#clean up columns
+BotReads.f <- BotReads.f[,c(24,2:22)]
+names(BotReads.f)[1] <- "Isolate"
+write.csv(BotReads.f, "BOTphenotypes.csv")
