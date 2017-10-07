bc2939ce395ccb4db636cabf594a3a70f036c968
Nicole E Soltis <ne.soltis@gmail.com>
2017-10-06 17:35:31 -0700

    botrydial GWAS plotting

---
+++ scripts/bigRRGWAS/05_formatBigRRouts_BOT.R
@@ -0,0 +1,82 @@
+#reformat bigRR output data
+#Nicole E Soltis
+
+#--------------------------------------------------------
+rm(list=ls())
+library(tidyr)
+#setwd("~/Documents/GitRepos/BcSolGWAS/data/SNP_files")
+setwd("~/Projects/BcAt_RNAGWAS/data/")
+#Import data
+#reorganize file Sl_LesionSize.HEM.csv
+HEMdat <- read.csv("BotGWAS/bigRR_MAF20_092517.HEM.csv")
+
+#first remove first 4 rows (threshold data)
+HEMdat <- HEMdat[,-c(1)]
+HEMthresh <- HEMdat[1:8,]
+HEMdat <- HEMdat[-c(1:8),]
+HEMdat2 <- HEMdat
+
+#split chromosome and segment
+names(HEMdat)
+unique(HEMdat$X.1)
+HEMdat2 <- separate (HEMdat, X.1, into = c("Chrom", "Segment", "Pos") )
+#double check
+unique(HEMdat2$Chrom)
+unique(HEMdat2$Segment)
+HEM.plotdata <- HEMdat2
+
+#Reformat Chromosomes and Positions
+HEM.plotdata$Chrom <- as.numeric(as.character(HEM.plotdata$Chrom))
+HEM.plotdata$Segment <- as.numeric(as.character(HEM.plotdata$Segment))
+HEM.plotdata$Pos <- as.numeric(as.character(HEM.plotdata$Pos))
+
+#sort dataframe rows in order of Chrom, then Pos
+HEM.plotdata <- HEM.plotdata[with(HEM.plotdata, order(Chrom, Segment, Pos)), ]
+
+#now make segments line up consecutively
+HEM.plotdata$Chrom.Seg <- paste(HEM.plotdata$Chrom, HEM.plotdata$Segment, sep=".")
+HEM.plotdata$Chrom.Seg <- as.numeric(HEM.plotdata$Chrom.Seg)
+
+#let's try making the chrom.seg integers so that R isn't confused
+unique(HEM.plotdata$Chrom.Seg)
+HEM.plotdata$Chrom.Seg.F <- as.factor(HEM.plotdata$Chrom.Seg)
+unique(HEM.plotdata$Chrom.Seg.F)
+recode.vars <- data.frame(OGvals = c(1,1.1,2,2.1,2.2,2.3,2.4,2.5, 3, 3.1, 3.2, 4,  4.1, 5,  5.1, 6,  6.1, 6.2, 6.3, 7,  7.1, 7.2, 8,  8.1, 8.2, 9,  9.1, 10, 10.1, 11, 11.1, 12, 12.1, 13, 13.1, 13.2, 14, 14.1, 14.2, 15, 15.1, 15.2, 15.3, 15.4, 16, 16.1, 16.2, 16.3, 16.4, 16.5, 16.6, 16.7, 16.8, 16.9, 16.11), newvals = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55))
+print(recode.vars)
+
+HEM.plotdata$Chrom.Seg.Int <- recode.vars$newvals[match(HEM.plotdata$Chrom.Seg.F, recode.vars$OGvals)]
+unique(HEM.plotdata$Chrom.Seg.Int)
+
+#Make plotting variables
+HEM.plotdata$Index = NA
+ticks = NULL
+lastbase = 0
+
+#Redo the positions to make them sequential		-- accurate position indexing
+for (i in unique(HEM.plotdata$Chrom.Seg.Int)) {
+  print(i)
+  #for chromosome 1
+  if (i==1) {
+    #for the subset of HEM.plotdata rows with Chromosome 1, set Index variable for each row to equal Pos.
+    HEM.plotdata[HEM.plotdata$Chrom.Seg.Int==i, ]$Index=HEM.plotdata[HEM.plotdata$Chrom.Seg.Int==i, ]$Pos
+    #for all other chromosomes: 
+  }	else {
+    #lastbase for chromosome i is the greater of:
+    #current lastbase counter plus the maxiumum position of chromosome i-1
+    #OR 1
+    #changed lastbase+tail to lastbase+max
+    lastbase=lastbase+max(subset(HEM.plotdata,HEM.plotdata$Chrom.Seg.Int==i-1)$Pos, 1)
+    #and then for the subset of HEM.plotdata rows with Chromosome i, set Index variable for each row to equal Pos + lastbase
+    HEM.plotdata[HEM.plotdata$Chrom.Seg.Int==i, ]$Index=HEM.plotdata[HEM.plotdata$Chrom.Seg.Int==i, ]$Pos+lastbase
+  }
+  #set ticks to be a list of existing ticks, plus the current Index
+  #floor rounds it down to the nearest whole number
+  # ticks=c(ticks, HEM.plotdata[HEM.plotdata$Chrom.Seg.Int==i, ]$Index[floor(length(HEM.plotdata[HEM.plotdata$Chrom==i, ]$Index)/2)+1])
+  
+  ticks=c(ticks, HEM.plotdata[HEM.plotdata$Chrom.Seg.Int==i, ]$Index[floor(length(HEM.plotdata[HEM.plotdata$Chrom.Seg.Int==i, ]$Index)/2)+1])
+}
+ticklim=c(min(HEM.plotdata$Index),max(HEM.plotdata$Index))
+
+write.csv(HEM.plotdata, "BcBOT_MAF20_HEM.PlotFormat.csv") 
+write.csv(HEMthresh, "BcBOT_MAF20_HEM.Thresh.csv")
+#read in to 06_plots
---
+++ scripts/bigRRGWAS/06_bigRRplots_BOT.MAF10.R
@@ -0,0 +1,133 @@
+#Nicole E Soltis 
+#from script by Jason A Corwin, Modified by Rachel Fordyce
+#to run bigRR on Linux GPU for GWAS
+#---------------------------------------------------------------
+
+rm(list=ls())
+setwd("~/Projects/BcAt_RNAGWAS/data/")
+
+#just Manhattan plots for individual plant genotypes
+############################################################################
+###Plotting the HEM results
+
+#NEED TO CHECK CONTIGS FOR THIS
+
+#Load plotting package
+library(ggplot2)
+library(grid)
+library(dplyr)
+
+#Import data (reorganized from script ReformatBigRRouts.R)
+HEM.plotdata.og <- read.csv("BcBOT_MAF10_HEM.PlotFormat.csv")
+
+HEM.plotdata <- HEM.plotdata.og
+
+HEM.plotdata <- HEM.plotdata[,-c(1)]
+
+#get threshhold values 
+HEM.thresh <- read.csv("BcBOT_MAF10_HEM.Thresh.csv")
+HEM.thresh <- HEM.thresh[,-c(1)]
+
+
+TH95pos <- HEM.thresh[1,]
+for (i in 2:ncol(TH95pos)){
+  assign(paste("TH95pos_", names(TH95pos[i]), sep=""),as.numeric(TH95pos[i]))
+}
+TH95neg <- HEM.thresh[5,]
+for (i in 2:ncol(TH95neg)){
+  assign(paste("TH95neg_", names(TH95neg[i]), sep=""),as.numeric(TH95neg[i]))
+}
+TH99pos <- HEM.thresh[3,]
+for (i in 2:ncol(TH99pos)){
+  assign(paste("TH99pos_", names(TH99pos[i]), sep=""),as.numeric(TH99pos[i]))
+}
+TH99neg <- HEM.thresh[7,]
+for (i in 2:ncol(TH99neg)){
+  assign(paste("TH99neg_", names(TH99neg[i]), sep=""),as.numeric(TH99neg[i]))
+}
+TH999pos <- HEM.thresh[4,]
+for (i in 2:ncol(TH999pos)){
+  assign(paste("TH999pos_", names(TH999pos[i]), sep=""),as.numeric(TH999pos[i]))
+}
+TH999neg <- HEM.thresh[8,]
+for (i in 2:ncol(TH999neg)){
+  assign(paste("TH999neg_", names(TH999neg[i]), sep=""),as.numeric(TH999neg[i]))
+}
+
+#create a custom color scale
+myColors <- c("grey40", "grey60", "grey40", "grey60", "grey40", "grey60", "grey40", "grey60", "grey40", "grey60", "grey40", "grey60", "grey40", "grey60", "grey40", "grey60")
+names(myColors) <- levels(HEM.plotdata$Chrom)
+colScale <- scale_colour_manual(name = "Chrom",values = myColors)
+
+#get midpoint positions per chromosome
+((max(HEM.plotdata[ which(HEM.plotdata$Chrom=='1'),]$Index) - min(HEM.plotdata[ which(HEM.plotdata$Chrom=='1'),]$Index))/2+min(HEM.plotdata[ which(HEM.plotdata$Chrom=='1'),]$Index))
+
+#get length per chromosome segment
+max(HEM.plotdata[which(HEM.plotdata$Chrom.Seg.F=='16.7'),]$Index) - min(HEM.plotdata[which(HEM.plotdata$Chrom.Seg.F=='16.7'),]$Index)
+
+#this is the correct list for NA20
+#c(1677875, )
+
+#try: keep top SNPs only
+for (i in c(4:14)){
+  assign(paste("HEMpos.", names(HEM.plotdata[i]), sep=""), subset(HEM.plotdata, HEM.plotdata[i] > get(paste("TH99pos_", names(HEM.plotdata[i]), sep="")), select=c(Chrom,Segment,Pos,Index,i)))
+  assign(paste("HEMneg.", names(HEM.plotdata[i]), sep=""), subset(HEM.plotdata, HEM.plotdata[i] < get(paste("TH99neg_", names(HEM.plotdata[i]), sep="")), select=c(Chrom,Segment,Pos,Index,i)))
+}
+
+#for top 1000 only
+for (i in c(4:14)){
+  assign(paste("HEMpos.", names(HEM.plotdata[i]), sep=""), head(arrange(get(paste("HEMpos.", names(HEM.plotdata[i]), sep="")), desc(get(paste("HEMpos.", names(HEM.plotdata[i]), sep=""))[,5])), n=500))
+  assign(paste("HEMneg.", names(HEM.plotdata[i]), sep=""), tail(arrange(get(paste("HEMneg.", names(HEM.plotdata[i]), sep="")), desc(get(paste("HEMneg.", names(HEM.plotdata[i]), sep=""))[,5])), n=500))
+}
+
+#combine pos and neg by group
+for (i in c(4:14)){
+  assign(paste("HEM.", names(HEM.plotdata[i]), sep=""), rbind(get(paste("HEMpos.", names(HEM.plotdata[i]), sep="")),get(paste("HEMneg.", names(HEM.plotdata[i]), sep=""))))
+}
+
+#then combine
+for (i in c(4:14)){
+  mydf <- paste("HEM.", names(HEM.plotdata[i]), sep="")
+  renamedf <- get(mydf)
+  colnames(renamedf)[5] <- "Effect"
+  assign(mydf, renamedf)
+  myblob <- rep(names(HEM.plotdata[i]), nrow(get(mydf)))
+  assign(mydf, cbind(get(mydf), Trait = myblob))
+}
+
+HEM.topSNPs <- rbind(HEM.Bcin12g06370.1.coi.1.HEM,HEM.Bcin12g06380.1.coi.1.HEM,HEM.Bcin12g06380.1.col.0.HEM,HEM.Bcin12g06390.1.coi.1.HEM,HEM.Bcin12g06400.1.coi.1.HEM,HEM.Bcin12g06400.1.col.0.HEM,HEM.Bcin12g06410.1.coi.1.HEM,HEM.Bcin12g06410.1.col.0.HEM,HEM.Bcin12g06420.1.coi.1.HEM,HEM.Bcin12g06430.1.coi.1.HEM,HEM.Bcin12g06430.1.col.0.HEM)
+
+#total sig SNPs per trait here
+table(HEM.topSNPs$Trait)
+
+plot(ggplot(HEM.topSNPs, aes(x=Index, y=Effect))+
+       theme_bw()+
+       geom_point(aes(color = factor(Trait))))
+
+
+#greyscale version
+#4 to 15
+for (i in c(4:24)){
+  #jpeg(paste("paper/plots/FigR6/bw_Sl_LesionSize_trueMAF20_NA10_lowTR_", names(HEM.plotdata[i]), ".ManhattanPlot.jpg", sep=""), width=7.5, height=5, units='in', res=600)
+  plot(ggplot(HEM.plotdata, aes(x=Index, y=100*HEM.plotdata[,i]))+
+         theme_bw()+
+         colScale+
+         geom_point(aes(color = factor(Chrom)))+
+         labs(list(y=expression(paste("Estimated Effect Size (",mm^{2},")")), title=paste("Lesion Size on ", names(HEM.plotdata[i]))))+
+         theme(text = element_text(size=14), axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))+
+         guides(col = guide_legend(nrow = 8, title="Chromosome"))+
+         geom_hline(yintercept=100*get(paste("TH95pos_", names(HEM.plotdata[i]), sep="")), colour = "black", lty=3) +
+         geom_hline(yintercept=100*get(paste("TH95neg_", names(HEM.plotdata[i]), sep="")), colour = "black", lty=3) +
+         geom_text(aes(0,100*get(paste("TH95neg_", names(HEM.plotdata[i]), sep="")), label = "95% Threshold", size=14, vjust = 2, hjust=.05), col = "black")+
+         geom_hline(yintercept=100*get(paste("TH99pos_", names(HEM.plotdata[i]), sep="")), lty=2) +
+         geom_hline(yintercept=100*get(paste("TH99neg_", names(HEM.plotdata[i]), sep="")), lty=2) +
+         geom_text(aes(0,100*get(paste("TH99neg_", names(HEM.plotdata[i]), sep="")), label = "99% Threshold", size=14, vjust = 4.5, hjust=.05), col = "black")+
+         theme(legend.position="none")+
+         theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+               panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+  )
+  #dev.off()
+}
+
+#NA10 chromosomes
+#scale_x_continuous(name="Chromosome", breaks = c(1674920, 5242762, 8999082, 11057880, 13580364, 17181767, 20009659, 22371413, 24388631, 26765466, 28559407, 30104939, 31864287, 33980281, 35775028, 38876579), labels = c("1", "2", "3", "4", "5", "6", "7","8", "9", "10", "11", "12", "13", "14", "15", "16"))+
