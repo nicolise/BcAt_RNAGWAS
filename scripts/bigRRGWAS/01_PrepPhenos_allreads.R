ea21dad80e141042c87aa49a47f86b705e977fc5
Nicole E Soltis <ne.soltis@gmail.com>
2018-01-19 17:02:31 -0800

    added lsmeans calculation for plant

--- scripts/bigRRGWAS/01_PrepPhenos_allreads.R
+++ scripts/bigRRGWAS/01_PrepPhenos_allreads.R
@@ -16,7 +16,8 @@ MyReads <- MyReads[,-c(1)]
 names(IsoNames)[1] <- "Isolate"
 IsoNames <- IsoNames[,c(1,3)]
 MyReads <- merge(MyReads, IsoNames, by="Isolate")
-MyReads <- MyReads[,c(1,2,9270,3:9269)]
+MyReads <- MyReads[,c(9270,2,3:9269)]
+names(MyReads)[1] <- "Isolate"
 
 #each plant x transcript is a unique phenotype
 #split by host
@@ -26,7 +27,27 @@ MyReads.coi1 <- splitlist[[1]]
 MyReads.col0 <- splitlist[[2]]
 MyReads.npr1 <- splitlist[[3]]
 
-#clean up columns
-MyReads.col0 <- MyReads.col0[,c(3,2,4:9270)]
-names(MyReads.col0)[1] <- "Isolate"
+#other approach: new lsmeans including the effect of HostGenotype
+
+full.lsm.outputs=NULL
+lsm.for.GWAS= as.data.frame(MyReads.lsm[,1])
+names(lsm.for.GWAS)[1] <- "Isolate"
+library(data.table)
+#run for 3:9269
+mytime1 <- (Sys.time())
+#takes 5 mins for 1000 -> predict 45 mins for 9k
+for (i in c(3:9269)) {
+  MyReads.lm <- lm(MyReads[,i] ~ Isolate + HostGenotype, data=MyReads)
+  MyReads.lsm <- as.data.frame(print(lsmeans(MyReads.lm, "Isolate")))
+  df <- as.data.frame(print(MyReads.lsm))
+  #setDT(df, keep.rownames = T)[]
+  df$transcript <- names(MyReads)[i]
+  full.lsm.outputs = rbind(full.lsm.outputs, df)
+  lsm.for.GWAS = cbind(lsm.for.GWAS,df$lsmean)
+  names(lsm.for.GWAS)[i-1] <- names(MyReads)[i]
+}
+print(mytime1)
+print(Sys.time())
+
+#write out file
 write.csv(MyReads.col0, "allreadsGWAS/01_prepFiles/col0_allreads.csv")
