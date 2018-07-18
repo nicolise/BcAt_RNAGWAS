46a2320951f84e769139bd91928a63f5760069a8
nicolise <ne.soltis@gmail.com>
2018-07-18 15:24:04 -0700

    prep pheno files Bc x 3, At x 3

--- scripts/GEMMA_each/A02_prepPhenos_Bc.R
+++ scripts/GEMMA_each/A02_prepPhenos_Bc.R
@@ -12,6 +12,7 @@ rm(list=ls())
 #4. RUN ./plink --noweb --file 01_PLINK/dpbinMAF20NA10 --maf 0.2 --make-bed --out binMAF20NA10 } do this ONCE. NEXT STEP is customized by ogphenos/ permutation
 #5. copy these files to GEMMA_eachBc_At/01_PLINK
 #6. run this script (A02_prepPhenos_Bc.R)
+
 ##start here
 #7. cd Documents/GitRepos/BcAt_RNAGWAS/data/B05_GEMMA/
 #8. copy edited .fam, original .bim, .bed to C03_runGEMMA/
@@ -24,48 +25,52 @@ rm(list=ls())
 
 setwd("~/Documents/GitRepos/BcAt_RNAGWAS")
 IsoNames <- read.csv("data/Vivian_Bc/IsolateKey_Vivian.csv")
-MyReads <- read.csv("data/Vivian_Bc/result.lsm.csv")
+MyReads <- read.csv("data/Vivian_Bc/result.lsm.csv") #9270 phenotypes
+MyReads <- MyReads[,-c(1)]
+#attach Isolate Names
+names(IsoNames)[1] <- "Isolate"
+IsoNames <- IsoNames[,c(1,3)]
+MyReads <- merge(MyReads, IsoNames, by="Isolate")
+MyReads <- MyReads[,c(9270,2:9269)]
+names(MyReads)[1] <- "Isolate"
 
 #myFAM is the PLINK output of converting *.ped and *.map (01_TABtoPEDnMAP.R) to *.bed and *.bim and *.fam
 myFAM <- read.table("data/GEMMA_eachAt_Bc/01_PLINK/binMAF20NA10.fam")
 #GEMMA only needs column 1 (individual ID), column 6 (phenotype)
 #n (num) with -n 1 means column 6 is pheno, -n 2 means 7… etc.
 
-##start here
-##first, split MyReads by plant accession. Then generate matching FAM files for each.
-#attach Isolate Names
-names(IsoNames)[1] <- "Isolate"
-IsoNames <- IsoNames[,c(1,3)]
-MyReads <- merge(MyReads, IsoNames, by="Isolate")
+#first, split MyReads by plant accession. Then generate matching FAM files for each.
+myread_coi1 <- MyReads[MyReads$HostGenotype=="coi.1",]
+myread_col0 <- MyReads[MyReads$HostGenotype=="col.0",]
+myread_npr1 <- MyReads[MyReads$HostGenotype=="npr.1",]
 
+##do this for each of above
+Phenos <- myread_npr1
 #col2 = V2 = Isolate
+Phenos <- Phenos[,c(1,3:length(Phenos))]
 names(Phenos)[1] <- "V2"
 
 Phenos_match <- Phenos[ order(Phenos$V2), ]
+#remove non-genotyped 01.02.13 from Phenos
+Phenos_match <- Phenos_match[!Phenos_match$V2 =="1.02.13",]
 myFAM_match <- myFAM
-myFAM_match$delete <- c(1:96)
+myFAM_match$delete <- c(1:95)
 myFAM_match <- myFAM_match[ order(myFAM_match$V2), ]
 
+## check that these are 0, 0, 95
 setdiff(myFAM_match$V2,Phenos_match$V2)
 setdiff(Phenos_match$V2,myFAM_match$V2)
 intersect(myFAM_match$V2,Phenos_match$V2)
 
-#so 1.01.12 and 1.02.13 need dummy variables
-#add an empty variable 1.02.12 to Phenos
-for (y in 2:length(Phenos_match)){
-  Phenos_match[97,y] <- mean(Phenos_match[,y], na.rm=TRUE)
-}
-levels(Phenos_match$V2) <- c(levels(Phenos_match$V2), "1.01.12")
-Phenos_match[97,1] <- "1.01.12"
-#reorder phenos
-Phenos_match <- Phenos_match[c(1:5,97,6:11,13:96),]
-
 #now add Phenos_match onto myFAM_match
-myFAM_match2 <- cbind(myFAM_match, Phenos_match)
-myFAM_match2 <- myFAM_match2[order(myFAM_match$delete),]
-myFAM_match2 <- myFAM_match2[,c(1:5,9:length(myFAM_match2))]
+myFAM_match2 <- merge(myFAM_match, Phenos_match, by="V2")
+myFAM_match2 <- myFAM_match2[order(myFAM_match2$delete),]
+#remove dummy phenotype (column 6)
+#and reorder V1:V5
+myFAM_match2 <- myFAM_match2[,c(2,1,3:5,8:length(myFAM_match2))]
 
+##be sure to move new *.fam into the correct directory!
 Sys.time()
-write.table(myFAM_match2, "data/B05_GEMMA/02_GEMMA/binMAF20NA10.fam", row.names=FALSE, col.names=TRUE)
-myFAM_check <- read.table("data/B05_GEMMA/02_GEMMA/binMAF20NA10.fam")
+write.table(myFAM_match2, "data/GEMMA_eachAt_Bc/02_GEMMA/binMAF20NA10.fam", row.names=FALSE, col.names=TRUE)
+myFAM_check <- read.table("data/GEMMA_eachAt_Bc/02_GEMMA/binMAF20NA10.fam")
 Sys.time()
--- scripts/GEMMA_each/A03_prepPhenos_At.R
+++ scripts/GEMMA_each/A03_prepPhenos_At.R
@@ -24,34 +24,44 @@ myFAM <- read.table("data/GEMMA_eachBc_At/01_PLINK/binMAF20NA10.fam")
 names(IsoNames)[1] <- "Isolate"
 IsoNames <- IsoNames[,c(1,3)]
 MyReads <- merge(MyReads, IsoNames, by="Isolate")
+MyReads <- MyReads[,c(23960,2,4:23959)]
+names(MyReads)[1] <- "Isolate"
 
+#first, split MyReads by plant accession. Then generate matching FAM files for each.
+myread_coi1 <- MyReads[MyReads$HostGenotype=="coi.1",]
+myread_col0 <- MyReads[MyReads$HostGenotype=="col.0",]
+myread_npr1 <- MyReads[MyReads$HostGenotype=="npr.1",]
+
+##do this for each of above
+Phenos <- myread_npr1
 #col2 = V2 = Isolate
-names(Phenos)[2] <- "V2"
+Phenos <- Phenos[,c(1,3:length(Phenos))]
+names(Phenos)[1] <- "V2"
 
+Phenos_match <- Phenos[ order(Phenos$V2), ]
+#remove non-genotyped 01.02.13 from Phenos
+Phenos_match <- Phenos_match[!Phenos_match$V2 =="1.02.13",]
+#and 94.4 and Control
+Phenos_match <- Phenos_match[!Phenos_match$V2 =="94.4",]
+Phenos_match <- Phenos_match[!Phenos_match$V2 =="Control",]
 myFAM_match <- myFAM
-Phenos_match <- Phenos
+myFAM_match$delete <- c(1:95)
+myFAM_match <- myFAM_match[ order(myFAM_match$V2), ]
 
+## check that these are 0, 0, 95
 setdiff(myFAM_match$V2,Phenos_match$V2)
 setdiff(Phenos_match$V2,myFAM_match$V2)
 intersect(myFAM_match$V2,Phenos_match$V2)
 
-#remove Control, 1.02.13, 94.4
-##check these
-Phenos_match <- Phenos_match[c(2:12,14:59,61:98),]
-
-#double check now and save original order for FAM
-myFAM_match$delete <- c(1:95)
-setdiff(myFAM_match$V2,Phenos_match$V2) #check: is zero
-setdiff(Phenos_match$V2,myFAM_match$V2) #check: is zero
-intersect(myFAM_match$V2,Phenos_match$V2)
-
-#match and return to original order
+#now add Phenos_match onto myFAM_match
 myFAM_match2 <- merge(myFAM_match, Phenos_match, by="V2")
 myFAM_match2 <- myFAM_match2[order(myFAM_match2$delete),]
-#remove column "delete" and dummy phenotype (V6) and reorder column V1, V2
-myFAM_match2 <- myFAM_match2[,c(2,1,3:5,9:ncol(myFAM_match2))]
+#remove dummy phenotype (column 6)
+#and reorder V1:V5
+myFAM_match2 <- myFAM_match2[,c(2,1,3:5,8:length(myFAM_match2))]
 
-setwd("~/Documents/GitRepos/BcAt_RNAGWAS/data")
-write.csv(myFAM_match2, "B05_GEMMA/C03_runGEMMA/binMAF20NA10_fam.csv")
-write.table(myFAM_match2, "B05_GEMMA/C03_runGEMMA/binMAF20NA10_allphenos.fam", row.names=FALSE, col.names=TRUE)
-#if randomizing, copy _allphenos.fam to _rand.fam and also copy 01/.bed and 01/.bim to 02/_rand.
+##be sure to move new *.fam into the correct directory!
+Sys.time()
+write.table(myFAM_match2, "data/GEMMA_eachBc_At/02_GEMMA/binMAF20NA10.fam", row.names=FALSE, col.names=TRUE)
+myFAM_check <- read.table("data/GEMMA_eachBc_At/02_GEMMA/binMAF20NA10.fam")
+Sys.time()
