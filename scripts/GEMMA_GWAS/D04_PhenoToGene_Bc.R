af204bec1ad018d23e514b0c7e8f57ba74873a7d
nicolise <ne.soltis@gmail.com>
2018-08-06 16:33:01 -0700

    extracting top SNPs from At- 3 genos

--- scripts/GEMMA_GWAS/D04_PhenoToGene_Bc.R
+++ scripts/GEMMA_GWAS/D04_PhenoToGene_Bc.R
@@ -5,7 +5,9 @@
 #match GEMMA phenotypes to Gene ID
 setwd("~/Documents/GitRepos/BcAt_RNAGWAS/data/B05_GEMMA_Bc")
 myPhenos <- read.table("02_GEMMA/binMAF20NA10.fam")
-nameslist <- myPhenos[1,6:length(myPhenos)]
+nameslist <- myPhenos[1,6:length(myPhenos)] 
+#check that V6 is a real phenotype
+nameslist[1,1:10] #yes
 myGenes <- as.data.frame(t(nameslist))
 names(myGenes)[1] <- "Gene"
 myGenes$pheno <- 1:nrow(myGenes)
@@ -23,6 +25,8 @@ write.csv(nameddat, "05_GEMMAsumm/GeneNames/AllBcgenes_top10SNP_MAF20NA10_GEMMA_
 setwd("~/Documents/GitRepos/BcAt_RNAGWAS/data/B05_GEMMA_Bc/B_permut/")
 myPhenos <- read.table("02_GEMMA/binMAF20NA10.fam")
 nameslist <- myPhenos[1,6:length(myPhenos)]
+#check that V6 is a real phenotype
+nameslist[1,1:10] #yes
 myGenes <- as.data.frame(t(nameslist))
 names(myGenes)[1] <- "Gene"
 myGenes$pheno <- 1:nrow(myGenes)
@@ -32,7 +36,30 @@ mydat <- NA
 
 setwd("/media/nesoltis/Soltis_AtBc_eQTL/BcAt_RNAGWAS/B05_GEMMA_Bc/Bc_permut/")
 #mydat <- read.table("05_GEMMAsumm/RawOuts/GEMMA_topSNPsample_zscale.txt", sep=",", row.names=NULL)
+##change these out to annotate all SNP summaries per GEMMA run
 mydat <- read.table("05_GEMMAsumm/RawOuts/GEMMA_topSNPsample_zscale.txt", sep=",")
 names(mydat)
 nameddat <- merge(mydat, myGenes, by = "pheno")
 write.csv(nameddat, "05_GEMMAsumm/GeneNames/GEMMA_top1SNPsample.csv")
+#--------------------------------------------------------------------
+#Bc_col0 / coi1 / npr1 
+setwd("/media/nesoltis/Soltis_AtBc_eQTL/BcAt_RNAGWAS/GEMMA_eachAt_Bc")
+myPhenos <- NULL
+nameddat <- NA
+mydat <- NA
+myPhenos <- read.table("npr1/02_GEMMA/binMAF20NA10.fam")
+nameslist <- myPhenos[1,6:length(myPhenos)]
+#check that V6 is a real phenotype
+nameslist[1,1:10] #yes
+myGenes <- as.data.frame(t(nameslist))
+names(myGenes)[1] <- "Gene"
+myGenes$pheno <- 1:nrow(myGenes)
+#start with 1 file. Loop over all files in directory later
+##change these out to annotate all SNP summaries per GEMMA run
+mydat <- read.table("06_GEMMAsumm/npr1_GEMMA_top1SNPsample.txt", sep=",", row.names=NULL)
+#, row.names=NULL
+names(mydat)
+nameddat <- merge(mydat, myGenes, by = "pheno")
+##match name here
+write.csv(nameddat, "06_GEMMAsumm/GeneNames/npr1_GEMMA_top1SNPsample.csv")
+#---------------------------------------------------------------
--- scripts/GEMMA_each/A04_extractGEMMA_At.R
+++ scripts/GEMMA_each/A04_extractGEMMA_At.R
@@ -1,5 +1,5 @@
 #Nicole E Soltis
-#07/02/18
+#08/
 
 #-------------------------------------------------------
 #location of all GEMMA runs for BcAtRNAseq
@@ -10,29 +10,27 @@
 #kmat script: 
 #script: /media/nesoltis/Soltis_AtBc_eQTL/BcAt_RNAGWAS/GEMMA_eachBc_At/A04_runGEMMA_kmat_3genos.sh
 
-
-##customize from here once runs are done
 rm(list=ls())
-setwd("/media/nesoltis/Data/Kliebenstein/Soltis/BcAt_RNAGWAS/B05_GEMMA_At")
-
+setwd("/media/nesoltis/Soltis_AtBc_eQTL/BcAt_RNAGWAS/GEMMA_eachBc_At")
 
 #read in individual GEMMA output files (1 per geno)
 #read in all files in folder by pattern matching
 #my.files <- list.files(pattern = ".assoc.txt")
-
+    
   #each phenotype
-  for (i in 1:23947){
+for (j in c("col0","coi1","npr1")){
+ for (i in 1:23947){
     #actually: 1:23947 for At
     Sys.time()
-    my_gemma <- read.table(paste("output/binMAF20NA10_",i,".assoc.txt", sep=""), header=TRUE)
+    my_gemma <- read.table(paste("output/",j,"/",j,"_MAF20NA10_",i,".assoc.txt", sep=""), header=TRUE)
     Sys.time()
     #takes 4 seconds to read 1 phenotype
     #times 24000 = 27 hours
     #take top 10 SNP/phenotype
     #also save top 1 SNP/ phenotype
+    my_gemma$pheno <- i
     my_gemma_top100 <- my_gemma[order(my_gemma$p_score),]
     my_gemma_top100 <- my_gemma_top100[1:100,]
-    my_gemma_top100$pheno <- i
     row.names(my_gemma_top100) <- c(1:100) + (100*(i-1))
     my_gemma_top10 <- my_gemma_top100[1:10,]
     my_gemma_top1 <- my_gemma_top10[1,]
@@ -42,14 +40,14 @@ setwd("/media/nesoltis/Data/Kliebenstein/Soltis/BcAt_RNAGWAS/B05_GEMMA_At")
     my_gemma.z <- my_gemma.z[abs(my_gemma.z$beta_z) > 4,]
     mylgsnp <- my_gemma[abs(my_gemma$beta) > 0.5,]
     #this gives an error but it's fine
-    try(ifelse( i == 1, write.table(my_gemma_top100, "C06_GEMMAsumm/GEMMA_top100SNPsample.txt", sep = ",", col.names = TRUE), write.table(my_gemma_top100, "C06_GEMMAsumm/GEMMA_top100SNPsample.txt", sep = ",", col.names = FALSE, append = TRUE)))
-    try(ifelse( i == 1, write.table(my_gemma_top10, "C06_GEMMAsumm/GEMMA_top10SNPsample.txt", sep = ",", col.names = TRUE), write.table(my_gemma_top10, "C06_GEMMAsumm/GEMMA_top10SNPsample.txt", sep = ",", col.names = FALSE, append = TRUE)))
-    try(ifelse( i == 1, write.table(my_gemma_top1, "C06_GEMMAsumm/GEMMA_top1SNPsample.txt", sep = ",", col.names = TRUE), write.table(my_gemma_top1, "C06_GEMMAsumm/GEMMA_top1SNPsample.txt", sep = ",", col.names = FALSE, append = TRUE)))
-    try(ifelse( i == 1, write.table(my_gemma.z, "C06_GEMMAsumm/GEMMA_topSNPsample_zscale.txt", sep = ",", col.names = TRUE), write.table(my_gemma.z, "C06_GEMMAsumm/GEMMA_topSNPsample_zscale.txt", sep = ",", col.names = FALSE, append = TRUE)))
-    try(ifelse( i == 1, write.table(mylgsnp, "C06_GEMMAsumm/GEMMA_top100_beta05SNP.txt", sep = ",", col.names = TRUE), write.table(mylgsnp, "C06_GEMMAsumm/GEMMA_top100_beta05SNP.txt", sep = ",", col.names = FALSE, append = TRUE)))
+    try(ifelse( i == 1, write.table(my_gemma_top100, paste("05_GEMMAsumm/",j,"_GEMMA_top100SNPsample.txt", sep=""), sep = ",", col.names = TRUE), write.table(my_gemma_top100, paste("05_GEMMAsumm/",j,"_GEMMA_top100SNPsample.txt", sep=""), sep = ",", col.names = FALSE, append = TRUE)))
+    try(ifelse( i == 1, write.table(my_gemma_top10, paste("05_GEMMAsumm/",j,"_GEMMA_top10SNPsample.txt", sep=""), sep = ",", col.names = TRUE), write.table(my_gemma_top10, paste("05_GEMMAsumm/",j,"_GEMMA_top10SNPsample.txt", sep=""), sep = ",", col.names = FALSE, append = TRUE)))
+    try(ifelse( i == 1, write.table(my_gemma_top1, paste("05_GEMMAsumm/",j,"_GEMMA_top1SNPsample.txt", sep=""), sep = ",", col.names = TRUE), write.table(my_gemma_top1, paste("05_GEMMAsumm/",j,"_GEMMA_top1SNPsample.txt", sep=""), sep = ",", col.names = FALSE, append = TRUE)))
+    try(ifelse( i == 1, write.table(my_gemma.z, paste("05_GEMMAsumm/",j,"_GEMMA_topSNPsample_zscale.txt", sep=""), sep = ",", col.names = TRUE), write.table(my_gemma.z, paste("05_GEMMAsumm/",j,"_GEMMA_topSNPsample_zscale.txt", sep=""), sep = ",", col.names = FALSE, append = TRUE)))
+    try(ifelse( i == 1, write.table(mylgsnp, paste("05_GEMMAsumm/",j,"_GEMMA_top100_beta05SNP.txt", sep=""), sep = ",", col.names = TRUE), write.table(mylgsnp, paste("05_GEMMAsumm/",j,"_GEMMA_top100_beta05SNP.txt", sep=""), sep = ",", col.names = FALSE, append = TRUE)))
     Sys.time()
   }
-
+}
 top1_SNP <- read.table("C06_GEMMAsumm/GEMMA_top1SNPsample.txt", sep = ",")
 #------------------------------------------------------------------------------------
 #more stuff here - extra code
