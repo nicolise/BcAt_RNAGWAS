d6f21525bd0ebdf6ef07adff38c0335082c5c18d
nicolise <ne.soltis@gmail.com>
2019-02-06 16:52:20 -0800

    writing

--- scripts/forMS/11_hotspotAnnotjunk.R
+++ scripts/forMS/11_hotspotAnnotjunk.R
@@ -1,7 +1,18 @@
 setwd("~/Projects/BcAt_RNAGWAS/paper/Tables/formatted")
-mygenes <- read.csv("HotspotTargets_At_allgenes.csv")
+mygenes <- read.csv("extra/HotspotTargets_At_allgenes.csv")
 mygo <- read.csv("extra/HotspotTargets_At_GOlist.csv")
 names(mygenes)[1] <- "Gene"
 names(mygo)[1] <- "Gene"
 mygo <- merge(mygo, mygenes, by="Gene")
-write.csv(mygo, "HotspotTargets_At_GOlist_peaksTagged.csv")
+write.csv(mygo, "extra/HotspotTargets_At_GOlist_peaksTagged.csv")
+
+commongo <- as.data.frame(table(mygo$Function))
+names(commongo)[1] <- "fulldata"
+blah <- split(mygo, mygo$eQTLpeak)
+for (i in 1:20){
+  bleh <- as.data.frame(blah[i])
+  names(bleh)[2] <- "Function"
+  bleh$Function <- droplevels(bleh$Function)
+  bleh <- as.data.frame(table(bleh$Function))
+  
+}
