c865b054b9d022bccde413b5219cb488afbe36b0
nicolise <ne.soltis@gmail.com>
2019-02-08 16:16:01 -0800

    writing

--- scripts/forMS/11_hotspotAnnotjunk.R
+++ scripts/forMS/11_hotspotAnnotjunk.R
@@ -8,11 +8,12 @@ write.csv(mygo, "extra/HotspotTargets_At_GOlist_peaksTagged.csv")
 
 commongo <- as.data.frame(table(mygo$Function))
 names(commongo)[1] <- "fulldata"
+
 blah <- split(mygo, mygo$eQTLpeak)
 for (i in 1:20){
   bleh <- as.data.frame(blah[i])
-  names(bleh)[2] <- "Function"
-  bleh$Function <- droplevels(bleh$Function)
-  bleh <- as.data.frame(table(bleh$Function))
-  
+  summ <- as.data.frame(table(bleh[,2]))
+  commongo <- cbind(commongo, summ[,2])
+  names(commongo)[2+i] <- paste0(names(bleh)[2])
 }
+write.csv(commongo, "extra/HotspotTargets_At_GOlist_commonterms.csv")
